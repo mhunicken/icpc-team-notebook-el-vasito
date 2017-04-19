bd7b5893ff8391ebfd8f212eaab8a228aef157ca
MATIAS JOSE HUNICKEN BERARDO <mjh0112@famaf.unc.edu.ar>
2017-04-19 10:59:12 -0300

    Minor changes in dynamic connectivity

--- graphs/dynamic_connectivity.cpp
+++ graphs/dynamic_connectivity.cpp
@@ -1,20 +1,20 @@
 struct UnionFind {
 	int n,comp;
-	vector<int> pre,si,c;
-	UnionFind(int n=0):n(n),comp(n),pre(n),si(n, 1){
-		fore(i,0,n)pre[i]=i;}
-	int find(int x){return x==pre[x]?x:find(pre[x]);}
+	vector<int> uf,si,c;
+	UnionFind(int n=0):n(n),comp(n),uf(n),si(n,1){
+		fore(i,0,n)uf[i]=i;}
+	int find(int x){return x==uf[x]?x:find(uf[x]);}
 	bool join(int x, int y){
 		if((x=find(x))==(y=find(y)))return false;
 		if(si[x]<si[y])swap(x,y);
-		si[x]+=si[y];pre[y]=x;comp--;c.pb(y);
+		si[x]+=si[y];uf[y]=x;comp--;c.pb(y);
 		return true;
 	}
 	int snap(){return c.size();}
 	void rollback(int snap){
 		while(c.size()>snap){
 			int x=c.back();c.pop_back();
-			si[pre[x]]-=si[x];pre[x]=x;comp++;
+			si[uf[x]]-=si[x];uf[x]=x;comp++;
 		}
 	}
 };
@@ -34,9 +34,9 @@ struct DynCon {
 	void remove(int x, int y){
 		if(x>y)swap(x,y);
 		q.pb((Query){DEL,x,y});
-		int prev=last[mp(x,y)];
-		mt[prev]=q.size()-1;
-		mt.pb(prev);
+		int pr=last[mp(x,y)];
+		mt[pr]=q.size()-1;
+		mt.pb(pr);
 	}
 	void query(){
 		q.pb((Query){QUERY,-1,-1});mt.pb(-1);}
@@ -45,18 +45,18 @@ struct DynCon {
 		fore(i,0,q.size())if(q[i].type==ADD&&mt[i]<0)mt[i]=q.size();
 		go(0,q.size());
 	}
-	void go(int l, int r){
-		if(l+1==r){
-			if(q[l].type == QUERY) // answer query using DSU
+	void go(int s, int e){
+		if(s+1==e){
+			if(q[s].type==QUERY) // answer query using DSU
 				printf("%d\n",dsu.comp);
 			return;
 		}
-		int s=dsu.snap(),m=(l+r)/2;
-		for(int i=r-1;i>=m;--i)if(mt[i]>=0&&mt[i]<l)dsu.join(q[i].x, q[i].y);
-		go(l,m);
-		dsu.rollback(s);
-		for(int i=m-1;i>=l;--i)if(mt[i]>=r)dsu.join(q[i].x, q[i].y);
-		go(m,r);
-		dsu.rollback(s);
+		int k=dsu.snap(),m=(s+e)/2;
+		for(int i=e-1;i>=m;--i)if(mt[i]>=0&&mt[i]<s)dsu.join(q[i].x,q[i].y);
+		go(s,m);
+		dsu.rollback(k);
+		for(int i=m-1;i>=s;--i)if(mt[i]>=e)dsu.join(q[i].x,q[i].y);
+		go(m,e);
+		dsu.rollback(k);
 	}
 };
