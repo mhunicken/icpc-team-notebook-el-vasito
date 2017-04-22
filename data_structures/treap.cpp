add2851132c5f8baeb0567498c6e7637c3d8b234
Matias Hunicken <matihunicken@gmail.com>
2017-04-22 00:49:59 -0300

    Treap (binary search tree)

---
+++ data_structures/treap.cpp
@@ -0,0 +1,45 @@
+typedef struct item *pitem;
+struct item {
+	int pr,key,cnt;
+	pitem l,r;
+	item(int key):key(key),pr(rand()),cnt(1),l(0),r(0) {}
+};
+int cnt(pitem t){return t?t->cnt:0;}
+void upd_cnt(pitem t){if(t)t->cnt=cnt(t->l)+cnt(t->r)+1;}
+void split(pitem t, int key, pitem& l, pitem& r){ // l: < key, r: >= key
+	if(!t)l=r=0;
+	else if(key<t->key)split(t->l,key,l,t->l),r=t;
+	else split(t->r,key,t->r,r),l=t;
+	upd_cnt(t);
+}
+void insert(pitem& t, pitem it){
+	if(!t)t=it;
+	else if(it->pr>t->pr)split(t,it->key,it->l,it->r),t=it;
+	else insert(it->key<t->key?t->l:t->r,it);
+	upd_cnt(t);
+}
+void merge(pitem& t, pitem l, pitem r){
+	if(!l||!r)t=l?l:r;
+	else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
+	else merge(r->l,l,r->l),t=r;
+	upd_cnt(t);
+}
+void erase(pitem& t, int key){
+	if(t->key==key)merge(t,t->l,t->r);
+	else erase(key<t->key?t->l:t->r,key);
+	upd_cnt(t);
+}
+pitem kth(pitem t, int k){
+	if(!t)return 0;
+	if(k==cnt(t->l))return t;
+	return k<cnt(t->l)?kth(t->l,k):kth(t->r,k-cnt(t->l)-1);
+}
+pair<int,int> lb(pitem t, int key){ // position and value of lower_bound
+	if(!t)return mp(0,1<<30); // (special value)
+	if(key>t->key){
+		auto w=lb(t->r,key);w.fst+=cnt(t->l)+1;return w;
+	}
+	auto w=lb(t->l,key);
+	if(w.fst==cnt(t->l))w.snd=t->key;
+	return w;
+}
---
+++ data_structures/treap_test.cpp
@@ -0,0 +1,81 @@
+// Timus 1439 - AC
+// http://acm.timus.ru/problem.aspx?space=1&num=1439
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
+using namespace std;
+typedef long long ll;
+
+typedef struct item *pitem;
+struct item {
+	int pr,key,cnt;
+	pitem l,r;
+	item(int key):key(key),pr(rand()),cnt(1),l(0),r(0) {}
+};
+int cnt(pitem t){return t?t->cnt:0;}
+void upd_cnt(pitem t){if(t)t->cnt=cnt(t->l)+cnt(t->r)+1;}
+void split(pitem t, int key, pitem& l, pitem& r){ // l: < key, r: >= key
+	if(!t)l=r=0;
+	else if(key<t->key)split(t->l,key,l,t->l),r=t;
+	else split(t->r,key,t->r,r),l=t;
+	upd_cnt(t);
+}
+void insert(pitem& t, pitem it){
+	if(!t)t=it;
+	else if(it->pr>t->pr)split(t,it->key,it->l,it->r),t=it;
+	else insert(it->key<t->key?t->l:t->r,it);
+	upd_cnt(t);
+}
+void merge(pitem& t, pitem l, pitem r){
+	if(!l||!r)t=l?l:r;
+	else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
+	else merge(r->l,l,r->l),t=r;
+	upd_cnt(t);
+}
+void erase(pitem& t, int key){
+	if(t->key==key)merge(t,t->l,t->r);
+	else erase(key<t->key?t->l:t->r,key);
+	upd_cnt(t);
+}
+pitem kth(pitem t, int k){
+	if(!t)return 0;
+	if(k==cnt(t->l))return t;
+	return k<cnt(t->l)?kth(t->l,k):kth(t->r,k-cnt(t->l)-1);
+}
+pair<int,int> lb(pitem t, int key){ // position and value of lower_bound
+	if(!t)return mp(0,1<<30); // (special value)
+	if(key>t->key){
+		auto w=lb(t->r,key);w.fst+=cnt(t->l)+1;return w;
+	}
+	auto w=lb(t->l,key);
+	if(w.fst==cnt(t->l))w.snd=t->key;
+	return w;
+}
+
+pitem ss;
+int n,q;
+
+int find(int x){
+	int s=1,e=n+1;
+	while(e-s>1){
+		int m=(s+e)/2;
+		if(m-lb(ss,m).fst>x)e=m;
+		else s=m;
+	}
+	return s;
+}
+
+int main(){
+	scanf("%d%d",&n,&q);
+	while(q--){
+		char c[4];int k;
+		scanf("%s%d",c,&k);
+		if(c[0]=='D')insert(ss,new item(find(k)));
+		else printf("%d\n",find(k));
+	}
+	fore(i,0,cnt(ss))assert(lb(ss,kth(ss,i)->key).fst==i);
+	return 0;
+}
