1ac7b1a8cf80d7ddccf91446d3a6bec5acb92f74
Martin <tinrodriguez8@gmail.com>
2017-04-28 13:00:10 -0300

    Add bit manipulation tricks

--- other/cpp_stuff.cpp
+++ other/cpp_stuff.cpp
@@ -9,4 +9,20 @@ struct comp {
 set<double,comp> w; // or map<double,int,comp>
 
 // Iterate over non empty subsets of bitmask
-for(int s=m;s;s=(s-1)&m)
+for(int s=m;s;s=(s-1)&m) // Decreasing order
+for (int s=0;s=s-m&m;) 	 // Increasing order
+
+// Return the numbers the numbers of 1-bit in x
+int __builtin_popcount (unsigned int x)
+
+// Returns the number of trailing 0-bits in x. x=0 is undefined.
+int __builtin_ctz (unsigned int x)
+
+// Returns the number of leading 0-bits in x. x=0 is undefined.
+int __builtin_clz (unsigned int x)
+
+// x of type long long just add 'll' at the end of the function.
+int __builtin_popcountll (unsigned long long x)
+
+// Get the value of the least significant bit that is one.
+v=(x&(-x))
