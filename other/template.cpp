a850dd7fbe832c62d627c97bab48fda01247c48f
Matias Hunicken <matihunicken@gmail.com>
2017-08-06 21:02:55 -0300

    Added template

---
+++ other/template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#ifdef DEMETRIO
+#define deb(...) fprintf(stderr,__VA_ARGS__)
+#define deb1(x) cerr << #x << " = " << x << endl
+#else
+#define deb(...) 0
+#define deb1(x) 0
+#endif
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
+#define SZ(x) ((int)x.size())
+using namespace std;
+typedef long long ll;
+
+int main(){
+	return 0;
+}
