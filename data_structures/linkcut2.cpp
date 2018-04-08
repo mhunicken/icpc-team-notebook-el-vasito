661ce98e3b3b3719a5c50e3bb9d011bd58944d5a
Ezekiel Carranza <ezekielcarranza@gmail.com>
2018-04-07 21:47:18 -0300

    Change names in linkcut2

--- data_structures/linkcut2.cpp
+++ data_structures/linkcut2.cpp
@@ -1,67 +1,66 @@
-const int N_DELTA = 0, N_VALUE = 0;
-inline int modifyOp(int x, int y){return x+y;}
-inline int queryOp(int lval, int rval){return lval + rval;}
-inline int dOnSeg(int d, int len){return d==N_DELTA ? N_DELTA : d*len;}
+const int N_DEL = 0, N_VAL = 0; //delta, value
+inline int mOp(int x, int y){return x+y;}//modify
+inline int qOp(int lval, int rval){return lval + rval;}//query
+inline int dOnSeg(int d, int len){return d==N_DEL ? N_DEL : d*len;}
 //mostly generic
-inline int joinVD(int v, int d){ return d==N_DELTA ? v : modifyOp(v, d);}
-inline int joinDeltas(int d1, int d2){
-  if(d1==N_DELTA)return d2; if(d2==N_DELTA)return d1; return modifyOp(d1, d2);
-}
+inline int joinD(int d1, int d2){
+  if(d1==N_DEL)return d2;if(d2==N_DEL)return d1;return mOp(d1, d2);}
+inline int joinVD(int v, int d){return d==N_DEL ? v : mOp(v, d);}
 struct Node_t{
-  int sz, nVal, stVal, d;
+  int sz, nVal, tVal, d;
   bool rev;
-  Node_t *ch[2], *p;
-  Node_t(int v) : sz(1), nVal(v), stVal(v), d(N_DELTA), rev(0), p(0){
-    ch[0]=ch[1]=0;
+  Node_t *c[2], *p;
+  Node_t(int v) : sz(1), nVal(v), tVal(v), d(N_DEL), rev(0), p(0){
+    c[0]=c[1]=0;
   }
-  bool isRoot(){return !p || (p->ch[0] != this && p->ch[1] != this);}
+  bool isRoot(){return !p || (p->c[0] != this && p->c[1] != this);}
   void push(){
     if(rev){
-      rev=0; swap(ch[0], ch[1]);
-      fore(x,0,2)if(ch[x])ch[x]->rev^=1;
+      rev=0; swap(c[0], c[1]);
+      fore(x,0,2)if(c[x])c[x]->rev^=1;
     }
-    nVal=joinVD(nVal, d); stVal=joinVD(stVal, dOnSeg(d, sz));
-    fore(x,0,2)if(ch[x])ch[x]->d=joinDeltas(ch[x]->d, d);
-    d=N_DELTA;
+    nVal=joinVD(nVal, d); tVal=joinVD(tVal, dOnSeg(d, sz));
+    fore(x,0,2)if(c[x])c[x]->d=joinD(c[x]->d, d);
+    d=N_DEL;
   }
   void upd();
 };
 typedef Node_t* Node;
 int getSize(Node r){return r ? r->sz : 0;}
-int getstVal(Node r){
-  return r ? joinVD(r->stVal, dOnSeg(r->d,r->sz)) : N_VALUE;}
+int getPV(Node r){
+  return r ? joinVD(r->tVal, dOnSeg(r->d,r->sz)) : N_VAL;}
 void Node_t::upd(){
-  stVal = queryOp(queryOp(getstVal(ch[0]), joinVD(nVal, d)), getstVal(ch[1]));
-  sz = 1 + getSize(ch[0]) + getSize(ch[1]);
+  tVal = qOp(qOp(getPV(c[0]), joinVD(nVal, d)), getPV(c[1]));
+  sz = 1 + getSize(c[0]) + getSize(c[1]);
 }
-void connect(Node ch, Node p, int isl){if(ch)ch->p=p; if(isl>=0)p->ch[1-isl]=ch;}
+void conn(Node c, Node p, int il){if(c)c->p=p;if(il>=0)p->c[!il]=c;}
 void rotate(Node x){
   Node p = x->p, g = p->p;
-  bool gCh=p->isRoot(), isl = x==p->ch[0];
-  connect(x->ch[isl],p,isl);connect(p,x,!isl);connect(x,g,gCh?-1:(p==g->ch[0]));
-  p->upd();
+  bool gCh=p->isRoot(), isl = x==p->c[0];
+  conn(x->c[isl],p,isl); conn(p,x,!isl);
+  conn(x,g,gCh?-1:(p==g->c[0])); p->upd();
 }
-void splay(Node x){
+void spa(Node x){//splay
   while(!x->isRoot()){
     Node p = x->p, g = p->p;
     if(!p->isRoot())g->push();
     p->push(); x->push();
-    if(!p->isRoot())rotate((x==p->ch[0])==(p==g->ch[0])? p : x);
+    if(!p->isRoot())rotate((x==p->c[0])==(p==g->c[0])? p : x);
     rotate(x);
   }
   x->push(); x->upd();
 }
-Node expose(Node x){
+Node exv(Node x){//expose
   Node last=0;
-  for(Node y=x; y; y=y->p)splay(y),y->ch[0]=last,y->upd(),last=y;
-  splay(x);
+  for(Node y=x; y; y=y->p)spa(y),y->c[0]=last,y->upd(),last=y;
+  spa(x);
   return last;
 }
-Node findRoot(Node x){expose(x); while(x->ch[1])x=x->ch[1]; splay(x); return x;}
-Node lca(Node x, Node y){expose(x); return expose(y);}
-void makeRoot(Node x){expose(x); x->rev^=1;}
-bool connected(Node x, Node y){if(x==y)return 1; expose(x);expose(y); return x->p;}
-void link(Node x, Node y){makeRoot(x); x->p=y;}
-void cut(Node x, Node y){makeRoot(x); expose(y); y->ch[1]->p = 0; y->ch[1]=0;}
-int query(Node x, Node y){makeRoot(x); expose(y); return getstVal(y);}
-void modify(Node x, Node y, int d){makeRoot(x); expose(y); y->d = joinDeltas(y->d, d);}
+void mkR(Node x){exv(x);x->rev^=1;}//makeRoot
+Node getR(Node x){exv(x);while(x->c[1])x=x->c[1];spa(x);return x;}
+Node lca(Node x, Node y){exv(x); return exv(y);}
+bool connected(Node x, Node y){exv(x);exv(y); return x==y?1:x->p!=0;}
+void link(Node x, Node y){mkR(x); x->p=y;}
+void cut(Node x, Node y){mkR(x); exv(y); y->c[1]->p=0; y->c[1]=0;}
+int query(Node x, Node y){mkR(x); exv(y); return getPV(y);}
+void modify(Node x, Node y, int d){mkR(x);exv(y);y->d=joinD(y->d,d);}
