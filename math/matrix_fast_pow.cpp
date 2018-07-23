a84cf8a946a550c1bc0a2faf2956d70aab3ec58a
Martin <tinrodriguez8@gmail.com>
2018-07-22 23:54:52 -0300

    Updated matrix files.

---
+++ math/matrix_fast_pow.cpp
@@ -0,0 +1,18 @@
+typedef vector<vector<ll> > Matrix;
+Matrix ones(int n) {
+	Matrix r(n,vector<ll>(n));
+	fore(i,0,n)r[i][i]=1;
+	return r;
+}
+Matrix operator*(Matrix &a, Matrix &b) {
+	int n=SZ(a),m=SZ(b[0]),z=SZ(a[0]);
+	Matrix r(n,vector<ll>(m));
+	fore(i,0,n)fore(j,0,m)fore(k,0,z)
+		r[i][j]+=a[i][k]*b[k][j],r[i][j]%=mod;
+	return r;
+}
+Matrix be(Matrix b, ll e) {
+	Matrix r=ones(SZ(b));
+	while(e){if(e&1LL)r=r*b;b=b*b;e/=2;}
+	return r;
+}
