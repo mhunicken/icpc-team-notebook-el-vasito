0a71b10a1a2ab00f8b0b752c6c1213444a4457d0
Matias Hunicken <matihunicken@gmail.com>
2018-10-26 23:25:55 -0300

    Erased extra lines in pollard rho

--- math/pollard_rho.cpp
+++ math/pollard_rho.cpp
@@ -83,35 +83,3 @@ void fact(ll n, map<ll,int>& f){ // call init_sv first!!!
 	else if(rabin(n))f[n]++;
 	else {ll q=rho(n);fact(q,f);fact(n/q,f);}
 }
-
-
-ll gcd(ll a, ll b){return a?gcd(b%a,a):b;}
-ll mulmod(ll a, ll b, ll m) {
-	if(!b)return 0;
-	ll q=mulmod(a,b/2,m);q=(q+q)%m;
-	return b&1?(q+a)%m:q;
-}
-ll expmod(ll b, ll e, ll m){
-	if(!e)return 1;
-	ll q=expmod(b,e/2,m);q=mulmod(q,q,m);
-	return e&1?mulmod(b,q,m):q;
-}
-bool is_prime_prob(ll n, int a){
-	if(n==a)return true;
-	ll s=0,d=n-1;
-	while(d%2==0)s++,d/=2;
-	ll x=expmod(a,d,n);
-	if((x==1)||(x+1==n))return true;
-	fore(_,0,s-1){
-		x=mulmod(x,x,n);
-		if(x==1)return false;
-		if(x+1==n)return true;
-	}
-	return false;
-}
-bool rabin(ll n){ // true iff n is prime
-	if(n==1)return false;
-	int ar[]={2,3,5,7,11,13,17,19,23};
-	fore(i,0,9)if(!is_prime_prob(n,ar[i]))return false;
-	return true;
-}
