bf0da9c2780e0682d0ae682032bef719ca742586
Ezekiel Carranza <ezekielcarranza@gmail.com>
2019-03-08 20:35:23 -0300

    Add Knuth condition to other

--- other/other.cpp
+++ other/other.cpp
@@ -2,3 +2,6 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 //int v=uniform_int_distribution<int>(a, b)(rng);
 
 //primitive root: a is pr of P if a^((P-1)/p)%P!=1 for all p|(P-1)
+
+//knuth: dp[i][j] = min(i<k<j) {dp[i][k]+dp[k][j]} + C[i][j]
+//A[i,j-1]<=A[i,j]<=A[i+1,j] (A is optimal position) reduces to O(n^2)
