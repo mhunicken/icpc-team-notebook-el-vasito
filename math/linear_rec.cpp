3fc12c0e1f77f20fd8350aad01ef45b653900b9c
Ezekiel Carranza <ezekielcarranza@gmail.com>
2019-03-08 17:45:56 -0300

    Add Berlekamp and LinearRec

---
+++ math/linear_rec.cpp
@@ -0,0 +1,30 @@
+//init O(n^2log) query(n^2 logk)
+//input: terms: first n term; trans: transition function; MOD; LOG=mxlog of k
+//output calc(k): kth term mod MOD
+//example: {1,1} {2,1} an=2*a_(n-1)+a_(n-2); calc(3)=3 calc(10007)=71480733
+struct LinearRec{
+  typedef vector<int> vi;
+  int n; vi terms, trans; vector<vi> bin;
+  vi add(vi &a, vi &b){
+    vi res(n*2+1);
+    fore(i,0,n+1)fore(j,0,n+1)res[i+j]=(res[i+j]*1LL+(ll)a[i]*b[j])%MOD;
+    for(int i=2*n; i>n; --i){
+      fore(j,0,n)res[i-1-j]=(res[i-1-j]*1LL+(ll)res[i]*trans[j])%MOD;
+      res[i]=0;
+    }
+    res.erase(res.begin()+n+1,res.end());
+    return res;
+  }
+  LinearRec(vi &terms, vi &trans):terms(terms),trans(trans){
+    vi a(n+1);a[1]=1;n=SZ(terms);
+    bin.pb(a);
+    fore(i,1,LOG)bin.pb(add(bin[i-1],bin[i-1]));
+  }
+  int calc(int k){
+    vi a(n+1);a[0]=1;
+    fore(i,0,LOG)if((k>>i)&1)a=add(a,bin[i]);
+    int ret=0;
+    fore(i,0,n)ret=((ll)ret+(ll)a[i+1]*terms[i])%MOD;
+    return ret;
+  }
+};
