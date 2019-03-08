f106fd0c6513e4f3f5010bf73086714ccf5cb923
Ezekiel Carranza <ezekielcarranza@gmail.com>
2019-03-08 18:01:39 -0300

    Change spaces in aho_corasick

--- strings/aho_corasick.cpp
+++ strings/aho_corasick.cpp
@@ -1,21 +1,14 @@
 struct vertex {
-	map<char,int> next,go;
-	int p,link;
-	char pch;
-	vector<int> leaf;
+	map<char,int> next,go;  int p,link;
+	char pch;  vector<int> leaf;
 	vertex(int p=-1, char pch=-1):p(p),pch(pch),link(-1){}
 };
 vector<vertex> t;
-void aho_init(){ //do not forget!!
-	t.clear();t.pb(vertex());
-}
+void aho_init(){t.clear();t.pb(vertex());} //DO NOT FORGET !!!
 void add_string(string s, int id){
 	int v=0;
 	for(char c:s){
-		if(!t[v].next.count(c)){
-			t[v].next[c]=t.size();
-			t.pb(vertex(v,c));
-		}
+		if(!t[v].next.count(c))t[v].next[c]=t.size(),t.pb(vertex(v,c));
 		v=t[v].next[c];
 	}
 	t[v].leaf.pb(id);
