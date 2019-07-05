8fbdcb47e770beeb96c0eb90ae4c824c3057f1a9
Marcos Kolodny <marcoskolodny@gmail.com>
2019-07-05 02:25:29 -0300

    changed mincost_maxflow implementation

--- flow/min_cost_max_flow.cpp
+++ flow/min_cost_max_flow.cpp
@@ -1,45 +1,50 @@
-typedef ll tf;const tf INFFLUJO=1e14;
-typedef ll tc;const tc INFCOSTO=1e14;
-struct edge {
-	int u,v;tf cap,flow;tc cost;
-	tf rem(){return cap-flow;}
-};
-int nodes; // remember to init nodes
-vector<int> g[MAXN];
-vector<edge> e;
-void add_edge(int u, int v, tf cap, tc cost) {
-	g[u].pb(SZ(e));e.pb((edge){u,v,cap,0,cost});
-	g[v].pb(SZ(e));e.pb((edge){v,u,0,0,-cost});
-}
-tc dist[MAXN],mncost;
-int pre[MAXN];
-tf cap[MAXN],mxflow;
-bool in_queue[MAXN];
-void flow(int s, int t){
-	memset(in_queue,0,sizeof(in_queue));
-	mxflow=mncost=0;
-	while(1){
-		fill(dist,dist+nodes,INFCOSTO);dist[s]=0;
-		memset(pre,-1,sizeof(pre));pre[s]=0;
-		memset(cap,0,sizeof(cap));cap[s]=INFFLUJO;
-		queue<int> q;q.push(s);in_queue[s]=1;
-		while(SZ(q)){
-			int u=q.front();q.pop();in_queue[u]=0;
-			fore(_,0,SZ(g[u])){
-				int i=g[u][_];
-				edge &E=e[i];
-				if(E.rem()&&dist[E.v]>dist[u]+E.cost+1e-9){
-					dist[E.v]=dist[u]+E.cost;
-					pre[E.v]=i;
-					cap[E.v]=min(cap[u],E.rem());
-					if(!in_queue[E.v])q.push(E.v),in_queue[E.v]=1;
+typedef ll tf;
+const tf INF=1e18;
+struct MCF{
+	int n;
+	vector<tf> prio,curflow,prevedge,prevnode,pot;
+	priority_queue<ll,vector<ll>,greater<ll>> q;
+	struct edge{int to, rev; tf f, cap, cost;};
+	vector<vector<edge>> g;
+	MCF(int n):n(n),prio(n),curflow(n),prevedge(n),prevnode(n),pot(n),g(n){}
+	void add_edge(int s, int t, tf cap, tf cost) {
+		g[s].pb((edge){t,SZ(g[t]),0,cap,cost});
+		g[t].pb((edge){s,SZ(g[s])-1,0,0,-cost});
+	}
+	pair<tf,tf> get_flow(int s, int t) {
+		tf flow=0, flowcost=0;
+		while(1){
+			q.push(s);
+			fill(ALL(prio),INF); 
+			prio[s]=0; curflow[s]=INF;
+			while(!q.empty()) {
+				ll cur=q.top();
+				int d=cur>>32, u=cur;
+				q.pop();
+				if(d!=prio[u]) continue;
+				for(int i=0; i<SZ(g[u]); ++i) {
+					edge &e=g[u][i];
+					int v=e.to;
+					if(e.cap<=e.f) continue;
+					tf nprio=prio[u]+e.cost+pot[u]-pot[v];
+					if(prio[v]>nprio) {
+						prio[v]=nprio;
+						q.push(((ll)nprio<<32)+v);
+						prevnode[v]=u; prevedge[v]=i;
+						curflow[v]=min(curflow[u], e.cap-e.f);
+					}
 				}
 			}
+			if(prio[t]==INF) break;
+			fore(i,0,n) pot[i]+=prio[i];
+			tf df=min(curflow[t], INF-flow);
+			flow+=df;
+			for(int v=t; v!=s; v=prevnode[v]) {
+				edge &e=g[prevnode[v]][prevedge[v]];
+				e.f+=df; g[v][e.rev].f-=df;
+				flowcost+=df*e.cost;
+			}
 		}
-		if(pre[t]<0)break;
-		mxflow+=cap[t];mncost+=cap[t]*dist[t];
-		for(int v=t;v!=s;v=e[pre[v]].u){
-			e[pre[v]].flow+=cap[t];e[pre[v]^1].flow-=cap[t];
-		}
+		return {flow,flowcost};
 	}
-}
+};
--- flow/min_cost_max_flow_test.cpp
+++ flow/min_cost_max_flow_test.cpp
@@ -1,92 +1,90 @@
 // SPOJ GREED - AC
-// http://www.spoj.com/problems/GREED/
+// https://www.spoj.com/problems/GREED/
 #include <bits/stdc++.h>
-#define pb push_back
-#define mp make_pair
 #define fst first
 #define snd second
-#define SZ(x) int((x).size())
-#define fore(i,a,b) for(int i=a,to=b;i<to;++i)
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
+#define pb push_back
+#define ALL(s) s.begin(),s.end()
+#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define SZ(s) int(s.size())
 using namespace std;
 typedef long long ll;
+typedef pair<int,int> ii;
 
-#define MAXN 512
-
-typedef int tf;const tf INFFLUJO=1e9;
-typedef int tc;const tc INFCOSTO=1e9;
-struct edge {
-	int u,v;tf cap,flow;tc cost;
-	tf rem(){return cap-flow;}
-};
-int nodes; // remember to init nodes
-vector<int> g[MAXN];
-vector<edge> e;
-void add_edge(int u, int v, tf cap, tc cost) {
-	g[u].pb(SZ(e));e.pb((edge){u,v,cap,0,cost});
-	g[v].pb(SZ(e));e.pb((edge){v,u,0,0,-cost});
-}
-tc dist[MAXN],mncost;
-int pre[MAXN];
-tf cap[MAXN],mxflow;
-bool in_queue[MAXN];
-void flow(int s, int t){
-	memset(in_queue,0,sizeof(in_queue));
-	mxflow=mncost=0;
-	while(1){
-		fill(dist,dist+nodes,INFCOSTO);dist[s]=0;
-		memset(pre,-1,sizeof(pre));pre[s]=0;
-		memset(cap,0,sizeof(cap));cap[s]=INFFLUJO;
-		queue<int> q;q.push(s);in_queue[s]=1;
-		while(SZ(q)){
-			int u=q.front();q.pop();in_queue[u]=0;
-			fore(_,0,SZ(g[u])){
-				int i=g[u][_];
-				edge &E=e[i];
-				if(E.rem()&&dist[E.v]>dist[u]+E.cost+1e-9){
-					dist[E.v]=dist[u]+E.cost;
-					pre[E.v]=i;
-					cap[E.v]=min(cap[u],E.rem());
-					if(!in_queue[E.v])q.push(E.v),in_queue[E.v]=1;
+typedef ll tf;
+const tf INF=1e18;
+struct MCF{
+	int n;
+	vector<tf> prio,curflow,prevedge,prevnode,pot;
+	priority_queue<ll,vector<ll>,greater<ll>> q;
+	struct edge{int to, rev; tf f, cap, cost;};
+	vector<vector<edge>> g;
+	MCF(int n):n(n),prio(n),curflow(n),prevedge(n),prevnode(n),pot(n),g(n){}
+	void add_edge(int s, int t, tf cap, tf cost) {
+		g[s].pb((edge){t,SZ(g[t]),0,cap,cost});
+		g[t].pb((edge){s,SZ(g[s])-1,0,0,-cost});
+	}
+	pair<tf,tf> get_flow(int s, int t) {
+		tf flow=0, flowcost=0;
+		while(1){
+			q.push(s);
+			fill(ALL(prio),INF); 
+			prio[s]=0; curflow[s]=INF;
+			while(!q.empty()) {
+				ll cur=q.top();
+				int d=cur>>32, u=cur;
+				q.pop();
+				if(d!=prio[u]) continue;
+				for(int i=0; i<SZ(g[u]); ++i) {
+					edge &e=g[u][i];
+					int v=e.to;
+					if(e.cap<=e.f) continue;
+					tf nprio=prio[u]+e.cost+pot[u]-pot[v];
+					if(prio[v]>nprio) {
+						prio[v]=nprio;
+						q.push(((ll)nprio<<32)+v);
+						prevnode[v]=u; prevedge[v]=i;
+						curflow[v]=min(curflow[u], e.cap-e.f);
+					}
 				}
 			}
+			if(prio[t]==INF) break;
+			fore(i,0,n) pot[i]+=prio[i];
+			tf df=min(curflow[t], INF-flow);
+			flow+=df;
+			for(int v=t; v!=s; v=prevnode[v]) {
+				edge &e=g[prevnode[v]][prevedge[v]];
+				e.f+=df; g[v][e.rev].f-=df;
+				flowcost+=df*e.cost;
+			}
 		}
-		if(pre[t]<0)break;
-		mxflow+=cap[t];mncost+=cap[t]*dist[t];
-		for(int v=t;v!=s;v=e[pre[v]].u){
-			e[pre[v]].flow+=cap[t];e[pre[v]^1].flow-=cap[t];
-		}
+		return {flow,flowcost};
 	}
-}
+};
 
 int q[512];
 int n;
 
-int main(){
-	int tn;
-	scanf("%d",&tn);
+int main(){FIN;
+	int tn; cin>>tn;
 	while(tn--){
-		scanf("%d",&n);
-		nodes=2+n;
+		cin>>n;
+		MCF nt(n+2);
 		memset(q,0,sizeof(q));
 		fore(i,0,n){
-			int x;
-			scanf("%d",&x);x--;
+			int x; cin >>x; x--;
 			q[x]++;
 		}
-		fore(i,0,n)if(q[i]>0)add_edge(0,2+i,q[i],0);
-		fore(i,0,n)add_edge(2+i,1,1,0);
-		int m;
-		scanf("%d",&m);
+		fore(i,0,n)if(q[i]>0)nt.add_edge(0,2+i,q[i],0);
+		fore(i,0,n)nt.add_edge(2+i,1,1,0);
+		int m; cin>>m;
 		while(m--){
-			int x,y;
-			scanf("%d%d",&x,&y);x--;y--;
-			add_edge(2+x,2+y,512,1);
-			add_edge(2+y,2+x,512,1);
+			int x,y; cin>>x>>y; x--; y--;
+			nt.add_edge(2+x,2+y,512,1);
+			nt.add_edge(2+y,2+x,512,1);
 		}
-		flow(0,1);
-		printf("%d\n",mncost);
-		fore(i,0,nodes)g[i].clear();
-		e.clear();
+		cout << nt.get_flow(0,1).snd<<"\n";
 	}
 	return 0;
 }
