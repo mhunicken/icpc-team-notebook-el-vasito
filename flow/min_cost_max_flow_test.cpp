0deae0b2b6a22700b75eaba76c4dbb44990ea443
Marcos Kolodny <marcoskolodny@gmail.com>
2020-05-27 15:03:55 -0300

    changed MCMF to allow flow with doubles

--- flow/min_cost_max_flow_test.cpp
+++ flow/min_cost_max_flow_test.cpp
@@ -13,45 +13,48 @@ typedef long long ll;
 typedef pair<int,int> ii;
 
 typedef ll tf;
-const tf INF=1e18;
+typedef ll tc;
+const tf INFFLOW=1e9;
+const tc INFCOST=1e9;
 struct MCF{
 	int n;
-	vector<tf> prio,curflow,prevedge,prevnode,pot;
-	priority_queue<ll,vector<ll>,greater<ll>> q;
-	struct edge{int to, rev; tf f, cap, cost;};
+	vector<tc> prio, pot; vector<tf> curflow; vector<int> prevedge,prevnode;
+	priority_queue<pair<tc, int>, vector<pair<tc, int>>, greater<pair<tc, int>>> q;
+	struct edge{int to, rev; tf f, cap; tc cost;};
 	vector<vector<edge>> g;
 	MCF(int n):n(n),prio(n),curflow(n),prevedge(n),prevnode(n),pot(n),g(n){}
-	void add_edge(int s, int t, tf cap, tf cost) {
+	void add_edge(int s, int t, tf cap, tc cost) {
 		g[s].pb((edge){t,SZ(g[t]),0,cap,cost});
 		g[t].pb((edge){s,SZ(g[s])-1,0,0,-cost});
 	}
-	pair<tf,tf> get_flow(int s, int t) {
-		tf flow=0, flowcost=0;
+	pair<tf,tc> get_flow(int s, int t) {
+		tf flow=0; tc flowcost=0;
 		while(1){
-			q.push(s);
-			fill(ALL(prio),INF); 
-			prio[s]=0; curflow[s]=INF;
+			q.push({0, s});
+			fill(ALL(prio),INFCOST); 
+			prio[s]=0; curflow[s]=INFFLOW;
 			while(!q.empty()) {
-				ll cur=q.top();
-				int d=cur>>32, u=cur;
+				auto cur=q.top();
+				tc d=cur.fst;
+        int u=cur.snd;
 				q.pop();
 				if(d!=prio[u]) continue;
 				for(int i=0; i<SZ(g[u]); ++i) {
 					edge &e=g[u][i];
 					int v=e.to;
 					if(e.cap<=e.f) continue;
-					tf nprio=prio[u]+e.cost+pot[u]-pot[v];
+					tc nprio=prio[u]+e.cost+pot[u]-pot[v];
 					if(prio[v]>nprio) {
 						prio[v]=nprio;
-						q.push(((ll)nprio<<32)+v);
+						q.push({nprio, v});
 						prevnode[v]=u; prevedge[v]=i;
 						curflow[v]=min(curflow[u], e.cap-e.f);
 					}
 				}
 			}
-			if(prio[t]==INF) break;
+			if(prio[t]==INFCOST) break;
 			fore(i,0,n) pot[i]+=prio[i];
-			tf df=min(curflow[t], INF-flow);
+			tf df=min(curflow[t], INFFLOW-flow);
 			flow+=df;
 			for(int v=t; v!=s; v=prevnode[v]) {
 				edge &e=g[prevnode[v]][prevedge[v]];
