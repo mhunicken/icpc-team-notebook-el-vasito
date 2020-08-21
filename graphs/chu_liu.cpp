79854765b3b20bc541c911bfa6d3e8758d3bb032
Marcos Kolodny <marcoskolodny@gmail.com>
2020-08-21 16:32:11 -0300

    Changed Chu Liu to allow spanning tree reconstruction

--- graphs/chu_liu.cpp
+++ graphs/chu_liu.cpp
@@ -1,54 +1,61 @@
-typedef ll tw;const tw INF=1LL<<60;
-struct edge {int src,dst;tw w;};
-struct ChuLiu {
-	int n,r;tw cost;bool found;
-	vector<int> no,pr,mark;
-	vector<vector<int> > comp,nx;
-	vector<tw> mcost;
-	vector<vector<edge> > h;
-	ChuLiu(int n):n(n),h(n){}
-	void add_edge(int x, int y, tw w){h[y].pb((edge){x,y,w});}
-	void visit(int v, int s){
-		if(mark[v]){
-			vector<int> temp=no;found=true;
-			do {
-				cost+=mcost[v];v=pr[v];
-				if(v!=s)while(comp[v].size()>0){
-					no[comp[v].back()]=s;
-					comp[s].pb(comp[v].back());
-					comp[v].pop_back();
-				}
-			}while(v!=s);
-			for(int j:comp[s])if(j!=r)for(edge& e:h[j])
-				if(no[e.src]!=s)e.w-=mcost[temp[j]];
-		}
-		mark[v]=true;
-		for(int i:nx[v])if(no[i]!=no[v]&&pr[no[i]]==v)
-			if(!mark[no[i]]||i==s)
-				visit(i,s);
+//O(n*m) minimum spanning tree in directed graph
+//returns -1 if not possible
+//included i-th edge if take[i]!=0
+typedef int tw; tw INF=1ll<<30;
+struct edge{int u,v,id;tw len;};
+struct ChuLiu{
+	int n; vector<edge> e;
+	vector<int> inc,dec,take,pre,num,id,vis;
+	vector<tw> inw;
+	void add_edge(int x, int y, tw w){
+		inc.pb(0); dec.pb(0); take.pb(0);
+		e.pb({x,y,SZ(e),w});
 	}
-	tw doit(int _r){ // r: root (O(nm))
-		r=_r;
-		no.resize(n);comp.clear();comp.resize(n);
-		fore(x,0,n)comp[x].pb(no[x]=x);
-		for(cost=0;;){
-			pr.clear();pr.resize(n,-1);
-			mcost=vector<tw>(n,INF);
-			fore(j,0,n)if(j!=r)for(edge e:h[j])
-				if(no[e.src]!=no[j]&&e.w<mcost[no[j]])
-					mcost[no[j]]=e.w,pr[no[j]]=no[e.src];
-			nx.clear();nx.resize(n);
-			fore(x,0,n)if(pr[x]>=0)nx[pr[x]].pb(x);
-			bool stop=true;
-			mark.clear();mark.resize(n);
-			fore(x,0,n)if(x!=r&&!mark[x]&&!comp[x].empty()){
-				found=false;visit(x,x);
-				if(found)stop=false;
+	ChuLiu(int n):n(n),pre(n),num(n),id(n),vis(n),inw(n){}
+	tw doit(int root){
+		auto e2=e;
+		tw ans=0; int eg=SZ(e)-1,pos=SZ(e)-1;
+		while(1){
+			fore(i,0,n) inw[i]=INF,id[i]=vis[i]=-1;
+			for(auto ed:e2) if(ed.len<inw[ed.v]){
+				inw[ed.v]=ed.len; pre[ed.v]=ed.u;
+				num[ed.v]=ed.id;
 			}
-			if(stop){
-				fore(x,0,n)if(pr[x]>=0)cost+=mcost[x];
-				return cost;
+			inw[root]=0;
+			fore(i,0,n) if(inw[i]==INF) return -1;
+			int tot=-1;
+			fore(i,0,n){
+				ans+=inw[i];
+				if(i!=root)take[num[i]]++;
+				int j=i;
+				while(vis[j]!=i&&j!=root&&id[j]<0)vis[j]=i,j=pre[j];
+				if(j!=root&&id[j]<0){
+					id[j]=++tot;
+					for(int k=pre[j];k!=j;k=pre[k]) id[k]=tot;
+				}
 			}
+			if(tot<0)break;
+			fore(i,0,n) if(id[i]<0)id[i]=++tot;
+			n=tot+1; int j=0;
+			fore(i,0,SZ(e2)){
+				int v=e2[i].v;
+				e2[j].v=id[e2[i].v];
+				e2[j].u=id[e2[i].u];
+				if(e2[j].v!=e2[j].u){
+					e2[j].len=e2[i].len-inw[v];
+					inc.pb(e2[i].id);
+					dec.pb(num[v]);
+					take.pb(0);
+					e2[j++].id=++pos;
+				}
+			}
+			e2.resize(j);
+			root=id[root];
 		}
+		while(pos>eg){
+			if(take[pos]>0) take[inc[pos]]++, take[dec[pos]]--;
+			pos--;
+		}
+		return ans;
 	}
 };
