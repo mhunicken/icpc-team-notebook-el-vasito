70acaecb483a7539699d304cf109cbd49f9402b5
Marcos Kolodny <marcoskolodny@gmail.com>
2020-09-16 20:12:39 -0300

    new version of halfplane intersection with less precision issues

--- geometry/halfplanes.cpp
+++ geometry/halfplanes.cpp
@@ -1,54 +1,32 @@
-int sgn3(double d){return abs(d)<EPS?0:(d>0?1:-1);}
+// polygon intersecting left side of halfplanes
 struct halfplane:public ln{
-    double angle;
-    halfplane(){}
-    halfplane(pt a,pt b){p=a; pq=b-a; angle=atan2(pq.y,pq.x);}
-    bool operator<(halfplane b)const{return angle<b.angle;}
+	double angle;
+	halfplane(){}
+	halfplane(pt a,pt b){p=a; pq=b-a; angle=atan2(pq.y,pq.x);}
+	bool operator<(halfplane b)const{return angle<b.angle;}
 };
-struct halfplanes {
-  int n;
-  vector<halfplane> hp;
-  halfplanes(vector<halfplane> v):hp(v),n(SZ(v)){}
-  halfplanes(){}
-  void normalize(){ //removes unnecesary lines and orders by angle
-    sort(hp.begin(),hp.end());
-    vector<halfplane> v = {hp[0]};
-    fore(i,1,n){
-      if(sgn3(hp[i].angle-v.back().angle)) v.pb(hp[i]);
-      else if(sgn3(v.back().pq%(hp[i].p-v.back().p))>0) v.back()=hp[i];
-    }
-    hp = v; n = hp.size();
-  }
-  // polygon intersecting left side of halfplanes
-  // may cause problems if EPS is too small
-  vector<pt> intersect(){
-    vector<pt>bx={{DINF,DINF},{-DINF,DINF},{-DINF,-DINF},{DINF,-DINF}};
-    fore(i,0,4) hp.pb(halfplane(bx[i],bx[(i+1)%4]));
-    n=SZ(hp);
-    normalize();
-    int st=0,ed=1;
-    vector<int> que(n);
-    vector<pt> p1(n);
-    que[st]=0; que[ed]=1;
-    p1[1]=hp[0]^hp[1];
-    for(int i=2; i<n && st <= ed;i++){
-      while(st<ed&&sgn3(((hp[i].pq)%(p1[ed]-hp[i].p)))<0)ed--;
-      while(st<ed&&sgn3(((hp[i].pq)%(p1[st+1]-hp[i].p)))<0)st++;
-      que[++ed]=i;
-      if(hp[i]/hp[que[ed-1]]) ed=-1;
-      else p1[ed]=hp[i]^hp[que[ed-1]];
-    }
-    while(st<ed&&sgn3((hp[que[st]].pq)%(p1[ed]-hp[que[st]].p))<0)ed--;
-    while(st<ed&&sgn3((hp[que[ed]].pq)%(p1[st+1]-hp[que[ed]].p))<0)st++;
-    vector<pt> ans;
-    if(st <= ed){
-      p1[st]=hp[que[st]]^hp[que[ed]];
-      fore(i,st,ed+1) ans.pb(p1[i]);
-    }
-    if(!SZ(ans)) return ans;
-    // change sign of EPS in point.left()
-    int f=1; for(auto x : hp) f &= ans[0].left(x.p,x.p+x.pq);
-    if(!f) ans.clear();
-    return ans;
-  }
-};
+vector<pt> intersect(vector<halfplane> &v){
+	vector<pt>bx={{DINF,DINF},{-DINF,DINF},{-DINF,-DINF},{DINF,-DINF}};
+	fore(i,0,4) v.pb(halfplane(bx[i],bx[(i+1)%4]));
+	sort(ALL(v));
+	int first=0,last=0,n=SZ(v);
+	vector<pt> p(n);
+	vector<halfplane> q(n);
+	q[first]=v[0];
+	fore(i,1,n){
+		while(first<last&&v[i].side(p[last-1])<0) last--;
+		while(first<last&&v[i].side(p[first])<0) first++;
+		q[++last]=v[i];
+		if(abs(q[last].pq%q[last-1].pq)<EPS){
+			last--;
+			if(q[last].side(v[i].p)>=0) q[last]=v[i];
+		}
+		if(first<last) p[last-1]=q[last-1]^q[last];
+	}
+	while(first<last&&q[first].side(p[last-1])<0) last--;
+	if(last-first<=1) return {};
+	p[last]=q[last]^q[first];
+	vector<pt> ans;
+	fore(i,first,last+1) ans.pb(p[i]);
+	return ans;
+}
--- geometry/halfplanes_test.cpp
+++ geometry/halfplanes_test.cpp
@@ -34,17 +34,29 @@ struct pt {
 };
 
 int sgn2(double x){return x<0?-1:1;}
-
 struct ln {
-  pt p,pq;
-  ln(pt p, pt q):p(p),pq(q-p){}
-  ln(){}
-  bool operator/(ln l){return abs(pq.unit()%l.pq.unit())<EPS;}
-  pt operator^(ln l){
-  if(*this/l)return pt(DINF,DINF);
-  pt r=l.p+l.pq*((p-l.p)%pq/(l.pq%pq));
-  return r;
-  }
+	pt p,pq;
+	ln(pt p, pt q):p(p),pq(q-p){}
+	ln(){}
+	bool has(pt r){return dist(r)<=EPS;}
+	bool seghas(pt r){return has(r)&&(r-p)*(r-(p+pq))<=EPS;}
+//	bool operator /(ln l){return (pq.unit()^l.pq.unit()).norm()<=EPS;} // 3D
+	bool operator/(ln l){return abs(pq.unit()%l.pq.unit())<=EPS;} // 2D
+	bool operator==(ln l){return *this/l&&has(l.p);}
+	pt operator^(ln l){ // intersection
+		if(*this/l)return pt(DINF,DINF);
+		pt r=l.p+l.pq*((p-l.p)%pq/(l.pq%pq));
+//		if(!has(r)){return pt(NAN,NAN,NAN);} // check only for 3D
+		return r;
+	}
+	int side(pt r){return has(r)?0:sgn2(pq%(r-p));} // 2D
+	pt proj(pt r){return p+pq*((r-p)*pq/pq.norm2());}
+	pt ref(pt r){return proj(r)*2-r;}
+	double dist(pt r){return (r-proj(r)).norm();}
+//	double dist(ln l){ // only 3D
+//		if(*this/l)return dist(l.p);
+//		return abs((l.p-p)*(pq^l.pq))/(pq^l.pq).norm();
+//	}
 };
 
 int sgn(double x){return x<-EPS?-1:x>EPS;}
@@ -60,60 +72,38 @@ struct pol {
   }
 };
 
-int sgn3(double d){return abs(d)<EPS?0:(d>0?1:-1);}
+// polygon intersecting left side of halfplanes
 struct halfplane:public ln{
-    double angle;
-    halfplane(){}
-    halfplane(pt a,pt b){p=a; pq=b-a; angle=atan2(pq.y,pq.x);}
-    bool operator<(halfplane b)const{return angle<b.angle;}
+	double angle;
+	halfplane(){}
+	halfplane(pt a,pt b){p=a; pq=b-a; angle=atan2(pq.y,pq.x);}
+	bool operator<(halfplane b)const{return angle<b.angle;}
 };
-struct halfplanes {
-  int n;
-  vector<halfplane> hp;
-  halfplanes(vector<halfplane> v):hp(v),n(SZ(v)){}
-  halfplanes(){}
-  void normalize(){ //removes unnecesary lines and orders by angle
-    sort(hp.begin(),hp.end());
-    vector<halfplane> v = {hp[0]};
-    fore(i,1,n){
-      if(sgn3(hp[i].angle-v.back().angle)) v.pb(hp[i]);
-      else if(sgn3(v.back().pq%(hp[i].p-v.back().p))>0) v.back()=hp[i];
-    }
-    hp = v; n = hp.size();
-  }
-  // polygon intersecting left side of halfplanes
-  // may cause problems if EPS is too small
-  vector<pt> intersect(){
-    vector<pt>bx={{DINF,DINF},{-DINF,DINF},{-DINF,-DINF},{DINF,-DINF}};
-    fore(i,0,4) hp.pb(halfplane(bx[i],bx[(i+1)%4]));
-    n=SZ(hp);
-    normalize();
-    int st=0,ed=1;
-    vector<int> que(n);
-    vector<pt> p1(n);
-    que[st]=0; que[ed]=1;
-    p1[1]=hp[0]^hp[1];
-    for(int i=2; i<n && st <= ed;i++){
-      while(st<ed&&sgn3(((hp[i].pq)%(p1[ed]-hp[i].p)))<0)ed--;
-      while(st<ed&&sgn3(((hp[i].pq)%(p1[st+1]-hp[i].p)))<0)st++;
-      que[++ed]=i;
-      if(hp[i]/hp[que[ed-1]]) ed=-1;
-      else p1[ed]=hp[i]^hp[que[ed-1]];
-    }
-    while(st<ed&&sgn3((hp[que[st]].pq)%(p1[ed]-hp[que[st]].p))<0)ed--;
-    while(st<ed&&sgn3((hp[que[ed]].pq)%(p1[st+1]-hp[que[ed]].p))<0)st++;
-    vector<pt> ans;
-    if(st <= ed){
-      p1[st]=hp[que[st]]^hp[que[ed]];
-      fore(i,st,ed+1) ans.pb(p1[i]);
-    }
-    if(!SZ(ans)) return ans;
-    // change sign of EPS in point.left()
-    int f=1; for(auto x : hp) f &= ans[0].left(x.p,x.p+x.pq);
-    if(!f) ans.clear();
-    return ans;
-  }
-};
+vector<pt> intersect(vector<halfplane> &v){
+	vector<pt>bx={{DINF,DINF},{-DINF,DINF},{-DINF,-DINF},{DINF,-DINF}};
+	fore(i,0,4) v.pb(halfplane(bx[i],bx[(i+1)%4]));
+	sort(ALL(v));
+	int first=0,last=0,n=SZ(v);
+	vector<pt> p(n);
+	vector<halfplane> q(n);
+	q[first]=v[0];
+	fore(i,1,n){
+		while(first<last&&v[i].side(p[last-1])<0) last--;
+		while(first<last&&v[i].side(p[first])<0) first++;
+		q[++last]=v[i];
+		if(abs(q[last].pq%q[last-1].pq)<EPS){
+			last--;
+			if(q[last].side(v[i].p)>=0) q[last]=v[i];
+		}
+		if(first<last) p[last-1]=q[last-1]^q[last];
+	}
+	while(first<last&&q[first].side(p[last-1])<0) last--;
+	if(last-first<=1) return {};
+	p[last]=q[last]^q[first];
+	vector<pt> ans;
+	fore(i,first,last+1) ans.pb(p[i]);
+	return ans;
+}
 
 int main(){FIN;
   int n; cin >> n;
@@ -124,6 +114,6 @@ int main(){FIN;
     fore(i,0,c) cin >> p[i].x >> p[i].y;
     fore(i,0,c) v.pb(halfplane(p[i],p[(i+1)%c]));
   }
-  pol p(halfplanes(v).intersect());
+  pol p(intersect(v));
   cout<<fixed<<setprecision(10)<<p.area()<<"\n";
 }
