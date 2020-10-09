4c02a9d36f1367023f8be1344624e1d180cde17a
Marcos Kolodny <marcoskolodny@gmail.com>
2020-10-09 19:45:19 -0300

    fixed typo in bellman ford

--- graphs/bellman_ford_test.cpp
+++ graphs/bellman_ford_test.cpp
@@ -19,7 +19,7 @@ vector<pair<int,int> > g[MAXN]; // u->[(v,cost)]
 ll dist[MAXN];
 void bford(int src){ // O(nm)
 	fill(dist,dist+n,INF);dist[src]=0;
-	fore(_,0,n-1)fore(x,0,n)if(dist[x]!=INF)for(auto t:g[x]){
+	fore(_,0,n)fore(x,0,n)if(dist[x]!=INF)for(auto t:g[x]){
 		dist[t.fst]=min(dist[t.fst],dist[x]+t.snd);
 	}
 	fore(x,0,n)if(dist[x]!=INF)for(auto t:g[x]){
