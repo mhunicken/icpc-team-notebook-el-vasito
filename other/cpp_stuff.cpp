4856f1f4816866cb2124cf5913cdb15c345795bb
Marcos Kolodny <marcoskolodny@gmail.com>
2021-09-04 20:05:54 -0300

    added knuth condition

--- other/cpp_stuff.cpp
+++ other/cpp_stuff.cpp
@@ -10,6 +10,8 @@ sys.stdout=sys.__stdout__
 //dp optimizations
 d&c: opt(i,j) <= opt(i,j+1)
 knuth: opt(i,j-1) <= opt(i,j) <= opt(i+1,j)
+dp[i][j] = min(k,C[i][j-1],C[i][j+1]+1)dp[i][k]+dp[k+1][j]+f(i,j)
+you only need to do dp[i][i]={NEUT,i} and dp[i][j]={val,C[i][j]};
 //pick theorem: Pick: A=I+B/2-1  (area, points inside, points on border)
 //generador random de int en rango [L,R], se puede cambiar por double/ll
 auto rnd=bind(uniform_int_distribution<int>(L,R), mt19937(time(0)));
