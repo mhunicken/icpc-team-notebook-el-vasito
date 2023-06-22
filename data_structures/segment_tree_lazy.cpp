2b55e48d92930abdb06762f18320d9a1d7e5d6f1
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2023-06-22 12:37:23 -0300

    Modularize operations in segment tree lazy

--- data_structures/segment_tree_lazy.cpp
+++ data_structures/segment_tree_lazy.cpp
@@ -1,41 +1,47 @@
 struct STree { // example: range sum with range addition
-	vector<int> st,lazy;int n;
-	STree(int n): st(4*n+5,0), lazy(4*n+5,0), n(n) {}
-	void init(int k, int s, int e, int *a){
-		lazy[k]=0;  // lazy neutral element
+	typedef int T; typedef int L; // T: data type, L: lazy type
+	constexpr static T tneut = 0; constexpr static L lneut = 0; // neutrals
+	T oper(T a, T b){return a+b;}
+	T apply(T v, L l, int s, int e) {return v+l*(e-s);} // new st according to lazy
+	L prop(L a, L b) {return a+b;} // cumulative effect of lazy
+
+	vector<T> st;vector<L> lazy;int n;
+	STree(int n): st(4*n+5,tneut), lazy(4*n+5,lneut), n(n) {}
+	void init(int k, int s, int e, const vector<T>& a){
+		lazy[k]=lneut;
 		if(s+1==e){st[k]=a[s];return;}
 		int m=(s+e)/2;
 		init(2*k,s,m,a);init(2*k+1,m,e,a);
-		st[k]=st[2*k]+st[2*k+1]; // operation
+		st[k]=oper(st[2*k],st[2*k+1]);
 	}
 	void push(int k, int s, int e){
-		if(!lazy[k])return; // if neutral, nothing to do
-		st[k]+=(e-s)*lazy[k]; // update st according to lazy
+		if(lazy[k] == lneut)return; // if neutral, nothing to do
+		st[k]=apply(st[k], lazy[k], s, e);
 		if(s+1<e){ // propagate to children
-			lazy[2*k]+=lazy[k];
-			lazy[2*k+1]+=lazy[k];
+			lazy[2*k]=prop(lazy[2*k],lazy[k]);
+			lazy[2*k+1]=prop(lazy[2*k+1],lazy[k]);
 		}
-		lazy[k]=0; // clear node lazy
+		lazy[k]=lneut; // clear node lazy
 	}
-	void upd(int k, int s, int e, int a, int b, int v){
+	void upd(int k, int s, int e, int a, int b, L v){
 		push(k,s,e);
 		if(s>=b||e<=a)return;
 		if(s>=a&&e<=b){
-			lazy[k]+=v; // accumulate lazy
+			lazy[k]=prop(lazy[k], v); // accumulate lazy
 			push(k,s,e);return;
 		}
 		int m=(s+e)/2;
 		upd(2*k,s,m,a,b,v);upd(2*k+1,m,e,a,b,v);
-		st[k]=st[2*k]+st[2*k+1]; // operation
+		st[k]=oper(st[2*k],st[2*k+1]);
 	}
-	int query(int k, int s, int e, int a, int b){
-		if(s>=b||e<=a)return 0; // operation neutral
+	T query(int k, int s, int e, int a, int b){
+		if(s>=b||e<=a)return tneut;
 		push(k,s,e);
 		if(s>=a&&e<=b)return st[k];
 		int m=(s+e)/2;
-		return query(2*k,s,m,a,b)+query(2*k+1,m,e,a,b); // operation
+		return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
 	}
-	void init(int *a){init(1,0,n,a);}
-	void upd(int a, int b, int v){upd(1,0,n,a,b,v);}
-	int query(int a, int b){return query(1,0,n,a,b);}
+	void init(const vector<T>& a){init(1,0,n,a);}
+	void upd(int a, int b, L v){upd(1,0,n,a,b,v);}
+	T query(int a, int b){return query(1,0,n,a,b);}
 }; // usage: STree rmq(n);rmq.init(x);rmq.upd(s,e,v);rmq.query(s,e);
