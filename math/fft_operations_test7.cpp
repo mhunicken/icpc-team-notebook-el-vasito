4710b739ae6a4cf355ac5a7ccc239cb9e0867035
Marcos Kolodny <marcoskolodny@gmail.com>
2026-04-02 23:39:58 +0200

    add test for chirp transform

---
+++ math/fft_operations_test7.cpp
@@ -0,0 +1,143 @@
+// Test for Chirp Z-transform
+// Yosupo library checker - AC
+// https://judge.yosupo.jp/problem/multipoint_evaluation_on_geometric_sequence
+#include <bits/stdc++.h>
+#define fst first
+#define snd second
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
+#define pb push_back
+#define ALL(s) s.begin(),s.end()
+#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define SZ(s) int(s.size())
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> ii;
+
+const int MOD=998244353;
+
+int addmod(int a, int b){a+=b;if(a>=MOD)a-=MOD;return a;}
+int submod(int a, int b){a-=b;if(a<0)a+=MOD;return a;}
+int mulmod(ll a, ll b){return a*b%MOD;}
+int fpow(int a, ll b){
+	int r=1;
+	while(b){if(b&1)r=mulmod(r,a); b>>=1; a=mulmod(a,a);}
+	return r;
+}
+int inv(int a){
+	return fpow(a,MOD-2);
+}
+
+// The maximum length of the resulting convolution vector is 2^LG
+const int LG = 21;
+typedef vector<ll> poly;
+
+template<class u, class uu, u p, u root>
+struct FFT {
+	u r[1+(2<<LG)];
+	constexpr u m(u a, u b) {
+		uu k = uu(a)*b;
+		#define op(g) g*(g*p+2)
+		k += u(k) * (op(op(op(op(op(-p)))))) * uu(p);
+		#undef op
+		return u(k>>(8*sizeof(u)));
+	}
+	constexpr u red(u k, u a){return a-k*(a>=k);}
+	FFT() {
+		u k=r[2<<LG]=-p%p, b=root, e=p>>LG;
+		for(;e;e/=2,b=m(b,b)) if(e&1) k=m(k,b);
+		for(int i=(2<<LG)-1;i>=0;i--) r[i]=red(p, m(r[i+1], k)), i&(i-1)?0:k=m(k,k);
+	}
+	poly cv(const poly &as, const poly &bs, u *v) {
+		int c=max(SZ(as)+SZ(bs)-1, 0), n=1;
+		assert(c <= (1<<LG));
+		u h=u(uu(-p)*-p%p), a=m(h, p/2+1), x, y;
+		while(n<c) n*=2, h=red(p, m(h, a));
+		fore(i,0,n){
+			v[i]=i<SZ(as)?u(as[i]):0,
+			v[i+n]=i<SZ(bs)?u(bs[i]):0;
+
+		}
+		for(auto s:{v,v+n}) for(int j=n;j>=2;j--) for(int k=j&-j; k/=2;) fore(i,j-k,j){
+			x=s[i], y=s[i-k];
+			s[i-k] = red(2*p, x+y);
+			s[i] = m(2*p+y-x, r[3*k-j+i]);
+		}
+		fore(i,0,n) v[i]=m(v[i], v[i+n]);
+		fore(j,2,n+1) for(int k=1; !(k&j); k*=2) fore(i,j-k,j){
+			x = m(v[i], r[3*k+j-i]);
+			y = red(2*p, v[i-k]);
+			v[i-k]=x+y, v[i]=2*p+y-x;
+		}
+		fore(i,0,c) v[i]=red(p, m(v[i], h));
+		return poly(v, v+c);
+	}
+};
+
+// For modular convolutions modulo an arbitrary 32-bit modulus.
+// Slightly slower than above versions, because it performs two calls to fft
+poly multiply(const poly v1, const poly v2, ll m=MOD) {
+	const uint64_t inv = 2703402103339935109ull,
+		mod1 = (1ull<<62)-(18ull<<32)+1,
+		mod2 = (1ull<<62)-(76ull<<32)+1;
+	static uint64_t v[2<<LG];
+	static FFT<uint64_t, __uint128_t, mod1, 3> fft1;
+	static FFT<uint64_t, __uint128_t, mod2, 17> fft2;
+	auto as=fft1.cv(v1, v2, v), bs=fft2.cv(v1, v2, v);
+	fore(i,0,SZ(as)){
+		auto d = fft1.m(mod1+as[i]-bs[i], inv);
+		d -= mod1*(d >= mod1); d %= m;
+		as[i] = (bs[i] + mod2%m*d)%m;
+	}
+	return as;
+}
+
+// Given any number g, perform multi-point polynomial evaluation
+// for all points of the form g^i, for each i in [0, k)
+// in O((n+k) * log(n+k))
+vector<int> chirpTransform(poly& p, int g, int k) {
+	int inv_g=inv(g), n=SZ(p)-1, sz=min(k,MOD-1);
+	poly ap(n+1, 0), bp(n+sz+1); 
+
+	fore(i,0,n+sz){
+		ll exp=1ll*i*(i-1)/2%(MOD-1);
+		if(i<=n) ap[n-i]=mulmod(p[i],fpow(inv_g, exp));
+		bp[i]=fpow(g, exp);
+	}
+
+	poly cp=multiply(ap, bp);
+
+	vector<int> ans(k);
+	int gk=1;
+	fore(i,0,sz){
+		ll exp=1ll*i*(i-1)/2%(MOD-1);
+		int val=0;
+		if(n+i<SZ(cp)) val=cp[n+i];
+		val=mulmod(val, fpow(inv_g,exp));
+		ans[i] = val;
+		gk=mulmod(gk,g);
+	}
+	fore(i,sz,k) ans[i]=ans[i-MOD+1];
+	return ans;
+}
+
+int main(){FIN;
+	int n,k,a,r; cin>>n>>k>>a>>r;
+
+	poly c(n);
+	fore(i,0,n) cin>>c[i];
+
+	int x=1;
+	fore(i,0,n) c[i]=mulmod(c[i],x), x=mulmod(x,a);
+	
+	vector<int> ans;
+
+	if(r==0){
+		ans=vector<int>(k,c[0]);
+		fore(i,1,n) ans[0]=addmod(ans[0], c[i]);
+	}
+	else{
+		ans=chirpTransform(c,r,k);
+	}
+
+	for(auto x:ans)cout<<x<<" ";cout<<"\n";
+}
